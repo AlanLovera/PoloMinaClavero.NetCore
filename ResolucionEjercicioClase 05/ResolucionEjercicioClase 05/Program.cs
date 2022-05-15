587093cb829001227a7c83c3f1301714669f455e
Alan Lovera <albra_lowe@live.com>
2022-05-15 01:47:34 -0300

    Resolucion Clase 05

---
+++ ResolucionEjercicioClase 05/ResolucionEjercicioClase 05/Program.cs
@@ -0,0 +1,32 @@
+﻿//Variable para inicializar el ciclo while
+var respuesta = "S";
+
+//Punto 5: Si la respuesta es "S" repetir desde el punt 1
+while (respuesta.ToUpper() == "S")
+{
+    //Punto 1: Borrar la pantalla
+    Console.Clear();
+
+    //Punto 2: Pedir el nombre de una persona
+    Console.WriteLine("Ingrese su nombre: ");
+    var nombre = Console.ReadLine();
+
+    //Punto 3: Saludarlo con un texto que diga "¡Hola [NombreIngrsado]!"
+    Console.WriteLine($"¡Hola {nombre}!");
+
+    //Punto 4: Preguntar si quiere continuar
+    Console.WriteLine("Desea continuar con el ciclo? (S/N)");
+    respuesta = Console.ReadLine();
+}
+
+//Punto 6: Si la respuesta es "N" finalizar un programa que diga: "Programa finalizado correctamente
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    /*Punto 7: Si la respuesta no es ni "S" ni "N" que también finalice el programa, pero mostrando un mensaje
+    que diga "Opcioón no válida" */
+    Console.WriteLine("Opción no válida");
+}
