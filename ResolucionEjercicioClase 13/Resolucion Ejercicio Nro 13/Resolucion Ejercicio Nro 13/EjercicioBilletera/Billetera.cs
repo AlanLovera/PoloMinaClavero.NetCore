41af744475247a339d7c33b92e5f56f66662b573
Alan Lovera <albra_lowe@live.com>
2022-06-22 00:24:10 -0300

    Resolucion Ejercicio 13

---
+++ ResolucionEjercicioClase 13/Resolucion Ejercicio Nro 13/Resolucion Ejercicio Nro 13/EjercicioBilletera/Billetera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resolucion_Ejercicio_Nro_13.EjercicioBilletera
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public Billetera(int billetesDe10, int billetesDe20, int billetesDe50, int billetesDe100, int billetesDe200, int billetesDe500, int billetesDe1000) {
+            BilletesDe10 = billetesDe10;
+            BilletesDe20 = billetesDe20;
+            BilletesDe50 = billetesDe50;
+            BilletesDe100 = billetesDe100;
+            BilletesDe200 = billetesDe200;
+            BilletesDe500 = billetesDe500;
+            BilletesDe1000 = billetesDe1000;
+
+        }
+        public int Total()
+        {
+            int total = BilletesDe10 * 10 + BilletesDe20 * 20 + BilletesDe50 * 50 + BilletesDe100 * 100 + BilletesDe200 * 200 + BilletesDe500 * 500 + BilletesDe1000 + 1000;
+            return total;
+        }
+
+    }
+}
---
+++ ResolucionEjercicioClase 13/Resolucion Ejercicio Nro 13/Resolucion Ejercicio Nro 13/Program.cs
@@ -0,0 +1,45 @@
+﻿using Resolucion_Ejercicio_Nro_13.EjercicioBilletera;
+
+var Billetera1 = new Billetera(3,4,6,8,9,3,5);
+Billetera1.BilletesDe10 = 3;
+Billetera1.BilletesDe20 = 5;
+Billetera1.BilletesDe50 = 5;
+Billetera1.BilletesDe100 = 7;
+Billetera1.BilletesDe200 = 9;
+Billetera1.BilletesDe500 = 5;
+Billetera1.BilletesDe1000 = 7;
+
+Console.WriteLine($"Billetes de 10: {Billetera1.BilletesDe10}");
+Console.WriteLine($"Billetes de 20: {Billetera1.BilletesDe20}");
+Console.WriteLine($"Billetes de 50: {Billetera1.BilletesDe50}");
+Console.WriteLine($"Billetes de 100: {Billetera1.BilletesDe100}");
+Console.WriteLine($"Billetes de 200: {Billetera1.BilletesDe200}");
+Console.WriteLine($"Billetes de 500: {Billetera1.BilletesDe500}");
+Console.WriteLine($"Billetes de 1000: {Billetera1.BilletesDe1000}");
+
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine("Billetera 1: ");
+Billetera1.Total();
+Console.WriteLine("---------------------------------------------------------");
+var Billetera2 = new Billetera(5,6,3,4,8,9,3);
+Billetera2.BilletesDe10 = 7;
+Billetera2.BilletesDe20 = 2;
+Billetera2.BilletesDe50 = 4;
+Billetera2.BilletesDe100 = 9;
+Billetera2.BilletesDe200 = 7;
+Billetera2.BilletesDe500 = 3;
+Billetera2.BilletesDe1000 = 6;
+
+Console.WriteLine($"Billetes de 10: {Billetera2.BilletesDe10}");
+Console.WriteLine($"Billetes de 20: {Billetera2.BilletesDe20}");
+Console.WriteLine($"Billetes de 50: {Billetera2.BilletesDe50}");
+Console.WriteLine($"Billetes de 100: {Billetera2.BilletesDe100}");
+Console.WriteLine($"Billetes de 200: {Billetera2.BilletesDe200}");
+Console.WriteLine($"Billetes de 500: {Billetera2.BilletesDe500}");
+Console.WriteLine($"Billetes de 1000: {Billetera2.BilletesDe1000}");
+Console.WriteLine("---------------------------------------------------------");
+
+Console.WriteLine("Billetera 2: ");
+Billetera2.Total();
+Console.WriteLine("---------------------------------------------------------");
